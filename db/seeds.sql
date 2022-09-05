288322ba201c0916c2938b337e766f34cdd4f006
FormulaXYZ <thomas.assefa6@gmail.com>
2022-09-04 21:05:52 -0700

    added schems.sql and seeds,sql

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('peter', 'sagan', 1, null),
+        ('eddy', 'merckk', 2, 1),
+        ('Dwight', 'Schrute', 2, 1),
+        ('greg', 'lemond', 4, 1),
+        ('jan', 'ullrich', 3, 1),
+        ('bernard', 'show', 5, 2),
+        ('chris', 'rock', 6, null);
+
+
+INSERT INTO department (department_name)
+VALUES ('Management'),
+       ('Human Resources'),
+       ('Sales'),
+       ('Accounting'),
+       ('Reception');
+
+
+INSERT INTO role (title, salary, department_id)
+  VALUES('CEO', 200000, null),
+        ('General Manager', 150000, 1),
+       ('Salesman', 60000, 2),
+       ('Accountant', 100000, 4),
+       ('Human Resource Officer', 82000, 5),
+       ('Receptionist', 52000, 3);
